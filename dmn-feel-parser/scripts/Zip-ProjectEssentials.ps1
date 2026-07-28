fa7d1544c6550e46d465fbd82cb1d5ffb89c85ae
konrad-link <konrad.link@gmail.com>
2026-07-28 19:43:09 +0200

    added docs site

--- dmn-feel-parser/scripts/Zip-ProjectEssentials.ps1
+++ dmn-feel-parser/scripts/Zip-ProjectEssentials.ps1
@@ -25,10 +25,7 @@ if (-not $projectName)
 $files = Get-ChildItem -Path $projectRoot -Recurse -File |
         Where-Object {
             $_.Name -eq "pom.xml" -or
-                    ($_.FullName -like "*\src\main\java\*") -or
-                    ($_.FullName -like "*\src\main\resources\*" -and
-                            $_.FullName -notlike "*\src\main\resources\swagger-ui\*" -and
-                            $_.FullName -notlike "*\src\main\resources\pdfjs\*")
+                    ($_.FullName -like "*\src\*")
         }
 
 # Prepare zip file path
--- dmn-protobuf/scripts/Zip-ProjectEssentials.ps1
+++ dmn-protobuf/scripts/Zip-ProjectEssentials.ps1
@@ -25,11 +25,7 @@ if (-not $projectName)
 $files = Get-ChildItem -Path $projectRoot -Recurse -File |
         Where-Object {
             $_.Name -eq "pom.xml" -or
-                    ($_.FullName -like "*\src\main\java\*") -or
-                    ($_.FullName -like "*\src\main\proto\*") -or
-                    ($_.FullName -like "*\src\main\resources\*" -and
-                            $_.FullName -notlike "*\src\main\resources\swagger-ui\*" -and
-                            $_.FullName -notlike "*\src\main\resources\pdfjs\*")
+                    ($_.FullName -like "*\src\main\*")
         }
 
 # Prepare zip file path
